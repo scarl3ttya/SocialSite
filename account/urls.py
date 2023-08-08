decfb5b7f8f8d2cfa543147cf389e559cb7dc409
yash <yas8830@yahoo.com>
2023-08-08 13:49:39 -0400

    Added login logout and passeord management

--- account/urls.py
+++ account/urls.py
@@ -2,12 +2,20 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
-app_name = 'account'
+#app_name = 'account'
 
 urlpatterns = [
-    path('', views.home, name='index'),
-    path('login/', views.user_login, name='login'),
-    #path('login/', auth_views.LoginView.as_view(), name='login'),
-    #path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    #path('', views.home, name='index'),
+    #path('login/', views.user_login, name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password-change', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password-reset', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/complete', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('', views.dashboard, name='dashboard'),
+    
     
 ]
